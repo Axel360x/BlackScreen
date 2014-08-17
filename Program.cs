91a93abc693ff1e3ad65f57fcaf4036086c45685
Axel360x <axel360x@gmail.com>
2014-08-17 15:20:39 +0200

    add
    add

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Blinking
+{
+    class Program
+    {
+        [DllImport("User32.dll")]
+        public static extern IntPtr GetDC(IntPtr hwnd);
+        [DllImport("User32.dll")]
+        public static extern void ReleaseDC(IntPtr hwnd, IntPtr dc);
+
+        public static void Main()
+        {
+            while (true)
+            {
+
+                IntPtr desktopPtr = GetDC(IntPtr.Zero);
+                Graphics g = Graphics.FromHdc(desktopPtr);
+
+                SolidBrush b = new SolidBrush(Color.Black);
+                g.FillRectangle(b, new Rectangle(0, 0, Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height));
+
+                g.Dispose();
+                ReleaseDC(IntPtr.Zero, desktopPtr);
+            }
+
+        }
+    }
+}
